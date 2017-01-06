2fd5ac3a0e972fce1ff6e8c694a2509441305c42
Vijayakumar Mahankali <vmahanka@us.ibm.com>
2017-01-06 14:53:27 -0800

    commit.

--- scala/psApp.scala
+++ scala/psApp.scala
@@ -1,3 +1,11 @@
 object HelloWorld2 extends App {
   println("Hello, world!")
 }
+
+object HelloWorld3 extends App {
+  println("Hello, world!")
+}
+
+object HelloWorld4 extends App {
+  println("Hello, world!")
+}
